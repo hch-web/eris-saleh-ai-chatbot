54f4a688ac3d6e5e1577befcc2427cbdb5cbf1d9
Haseeb@mac <haseeb@beyonderissolutions.com>
2023-09-25 17:43:37 +0500

    styles Bootstrap removed

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const el = document.createElement('div');
