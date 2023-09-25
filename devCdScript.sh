f95b75c648f34d23e1bc3b8b99f49204e258c76f
Ubuntu <ubuntu@ip-172-31-11-117.me-south-1.compute.internal>
2023-09-25 13:45:21 +0000

    Pipeline is added

---
+++ devCdScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/ubuntu/eris-ai-frontend;
+
+git pull;
+
+if [ $? -eq 0 ]; then
+  echo 'git pull success.'
+else
+  echo 'git pull failure.'
+  exit 1;
+fi
+
+sudo rm /var/www/html/scripts/bundle.js
+
+sudo cp /home/ubuntu/eris-ai-frontend/bundle/bundle.js /var/www/html/scripts
