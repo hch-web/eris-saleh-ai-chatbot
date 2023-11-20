0873534b91f561557167207ceee2cdca90367864
Haseeb Ahmed Chaudhary <haseeb@beyonderissolutions.com>
2023-11-20 20:17:57 +0500

    API URL domain updated

--- src/containers/services/chat.js
+++ src/containers/services/chat.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://157.241.54.34:9000/api/v1';
+const API_URL = 'https://chat-tts.beyonderissolutions.com/api/v1';
 
 export const getVoiceAudio = async text => {
   const response = await axios.post(`${API_URL}/speech`, { text });
